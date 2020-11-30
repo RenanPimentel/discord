c918c5ac7dae708e4d436dd24a735ed0b05d0e93
RenanSinhorini <renansinhorini12@gmail.com>
2020-11-30 13:44:11 -0300

    fix bug

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const client = new Discord.Client();
 
 const ready = require('./js/readyFunction');
 const message = require('./js/messageFunction');
-const calc = require('./js/calc');
-const permutation = require('./js/permutacao');
-const bomdia = 'https://cdn.discordapp.com/attachments/730098535558545461/782736100171055124/Humanas.jpg';
 
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
@@ -14,25 +11,13 @@ String.prototype.capitalize = function() {
 client.on('ready', () => ready(client));
 
 client.on('message', msg => {
-    const help = [
-        'ajuda',
-        'bomdia',
-        'calc',
-        'permutar'
-    ];
-    const helpFormatted = `${process.env.prefix}${help.join(`\n${process.env.prefix}`)}`;
+    if (msg.author.bot) return;
+    
+    checkForMario(msg);
 
-    const commandMsg = msg.content.slice(process.env.prefix.length).trim();
-    const args = commandMsg.split(/ /g);
+    if (!msg.content.startsWith(process.env.prefix)) return;
 
-    const commands = {
-        [help[0]]: { value: helpFormatted, img: false },
-        [help[1]]: { value: bomdia, img: true },
-        [help[2]]: { value: calc(args), img: false },
-        [help[3]]: { value: permutation(args), img: false }
-    };
-
-    message(msg, commands, args, Discord);
+    message(msg);
 });
 
 client.login(process.env.token);
--- js/messageFunction.js
+++ js/messageFunction.js
@@ -3,22 +3,36 @@ const randColor = require("./randomColor");
 const rand = require('./randomNumber');
 const makeEmbed = require("./makeEmbed");
 const checkForMario = require('./mario');
+const calc = require('./calc');
+const permutation = require('./permutacao');
 
+const bomdia = 'https://cdn.discordapp.com/attachments/730098535558545461/782736100171055124/Humanas.jpg';
 const paiOn = 'https://cdn.discordapp.com/attachments/218472696030298112/782767656096366622/paitaon.png';
 
-function msgFunction(msg, commands, args) {
-    if (msg.author.bot) return;
-    
-    checkForMario(msg);
+function msgFunction(msg) {
+    const help = [
+        'ajuda',
+        'bomdia',
+        'calc',
+        'permutar'
+    ];
+    const helpFormatted = `${process.env.prefix}${help.join(`\n${process.env.prefix}`)}`;
 
-    if (!msg.content.startsWith(process.env.prefix)) return;
-
+    const commandMsg = msg.content.slice(process.env.prefix.length).trim();
+    const args = commandMsg.split(/ /g);
     const command = args.shift().toLowerCase().trim();
+
+    const commands = {
+        [help[0]]: { value: helpFormatted, img: false },
+        [help[1]]: { value: bomdia, img: true },
+        [help[2]]: { value: calc(args), img: false },
+        [help[3]]: { value: permutation(args), img: false }
+    };
 
     if (command in commands) {
         const title = command.capitalize().trim() + ' ' + args.join(' ').trim();
         const randImgs = imgs[rand(0, imgs.length)];
-        const commandValue = commands[command].value;    
+        const commandValue = commands[command].value;
 
         if (!commands[command].img) {
             const embed = makeEmbed(title, commandValue, randColor(), paiOn, randImgs);
